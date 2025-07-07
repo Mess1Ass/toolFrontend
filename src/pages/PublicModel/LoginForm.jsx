11049b4d7d172499ed24b5a86a8ac4f0c34b51fa
guanzy <1783806041@qq.com>
2025-07-07 10:32:51 +0800

    更新接口2.0（后端取消域名 前端接口更改）

--- src/pages/PublicModel/LoginForm.jsx
+++ src/pages/PublicModel/LoginForm.jsx
@@ -38,7 +38,7 @@ function LoginForm({ onLoginSuccess }) {
       formData.append('password', password);
 
       //const response = await axios.post('http://localhost:5000/api/login', formData, {
-      const response = await axios.post('http://api.tool4me.cn/api/login', formData, {
+      const response = await axios.post('http://106.14.212.1:5000/api/login', formData, {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         }
