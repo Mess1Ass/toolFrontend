c0e3d1d5ecd805b55e722970d6e91224bb39f6b5
guanzy <1783806041@qq.com>
2025-07-25 09:42:13 +0800

    接口更新测试

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 // src/config.js
 
-const ENV = "dev"; // 切换成 "prod" 使用服务器地址
-//const ENV = "prod"; 
+// const ENV = "dev"; // 切换成 "prod" 使用服务器地址
+const ENV = "prod"; 
 
 const config = {
   dev: {
     API_BASE_URL: "http://localhost:5000"
   },
   prod: {
-    API_BASE_URL: "http://106.14.212.1:5000"
+    API_BASE_URL: "https://106.14.212.1:5000"
   }
 };
 
